7d4ffeb7a4c3050e4aad438bf0dcc593b45c89a0
Shanu Kumar <shanukumar467@gmail.com>
2023-05-01 23:13:06 +0530

    product component updated

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,32 @@
-import React from 'react';
-
+import React,{useContext} from 'react';
+import { ProductContext } from '../contexts/ProductContext';
+import Product from '../components/Product'
 const Home = () => {
-  return <div>Homepage</div>;
+  // Get Products 
+  const {products} = useContext(ProductContext)
+ // Get only mens and womens clothes catrgory
+ const filteredProducts = products.filter(product => {
+  return product.category === "men's clothing" || product.category === "women's clothing"
+ })
+ console.log(filteredProducts)
+  return ( 
+    <div>
+      <section className='py-16 '>
+        <div className='container mx-auto'>
+          <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-5 gap-[30px] max-w-sm mx-auto md:max-w-none md:mx-0'>
+            {
+              filteredProducts.map((product, i) => {
+                return(
+                 <Product key={product.id} product ={product} />
+                )
+              })
+            }
+          </div>
+
+        </div>
+      </section>
+    </div>
+  )
 };
 
 export default Home;
