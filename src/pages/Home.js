85805fcd7e8efde305e15c84161a0458e4f0f89e
Shanu Kumar <shanukumar467@gmail.com>
2023-05-04 10:12:41 +0530

    Hero component added

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,8 @@
 import React,{useContext} from 'react';
 import { ProductContext } from '../contexts/ProductContext';
 import Product from '../components/Product'
+import Hero from "../components/Hero"
+
 const Home = () => {
   // Get Products 
   const {products} = useContext(ProductContext)
@@ -11,6 +13,7 @@ const Home = () => {
  console.log(filteredProducts)
   return ( 
     <div>
+      <Hero />
       <section className='py-16 '>
         <div className='container mx-auto'>
           <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-5 gap-[30px] max-w-sm mx-auto md:max-w-none md:mx-0'>
