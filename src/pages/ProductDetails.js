a43a792293aa70ff63adc007e63a734a4cd33a65
Shanu Kumar <shanukumar467@gmail.com>
2023-05-04 23:07:59 +0530

    productDetails components added

--- src/pages/ProductDetails.js
+++ src/pages/ProductDetails.js
@@ -1,7 +1,51 @@
-import React from 'react';
+import React, { useContext } from "react";
+import { useParams } from "react-router-dom";
+import { CartContext } from "../contexts/CartContext";
+import { ProductContext } from "../contexts/ProductContext";
 
 const ProductDetails = () => {
-  return <div>Product Details Page</div>;
+  const { id } = useParams();
+  const { products } = useContext(ProductContext);
+  const { addToCart} = useContext(CartContext);
+console.log(products)
+
+
+const product = products.find((product) => {
+  return product.id === parseInt(id);
+});
+// If product is not found
+if (!product) {
+  return (
+    <section className="h-screen flex justify-center items-center">
+        Loading....
+      </section>
+    );
+  }
+  //Destructure products
+  const {  title, image, price, description } = product;
+  return (
+    <section className="pt-32 pb-12 lg:py-32 h-screen flex items-center">
+      <div className="container mx-auto">
+        <div className="flex flex-col lg:flex-row items-center">
+          {/* Image  */}
+          <div className="flex flex-1 justify-center items-center mb-8 lg:mb-0">
+            <img className="max-w-[200px] lg:max-w-sm" src={image} alt="product-image"/>
+          </div>
+          {/* Text  */}
+          <div className="flex-1 text-center lg:text-left">
+            <h1 className="text-[26px] font-medium mb-2 max-w-[450px] mx-auto lg:mx-0">{title}</h1>
+         {/* Price  */}
+         <div className="text-xl text-red-500 font-medium mb-6">$ {price}</div>
+         {/* Description  */}
+         <p className="mb-8">{description}</p>
+         <button onClick={() => addToCart(product, product.id)} className="bg-primary py-4 px-8 text-white ">
+          Add To Cart
+         </button>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default ProductDetails;
